4af9aa3d7e04984a93fdc5b7849bd5cf5591d941
Antonio Paulino <tonnyarauujo@gmail.com>
2022-03-29 11:06:44 -0300

    exercise crud with views

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,45 @@
+class ExercisesController < ApplicationController
+  before_action :set_exercise, only: %i[ show edit update destroy ]
+
+  def index
+    @exercises = Exercise.all
+  end
+
+  def show
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def edit
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+
+    return render :new, status: :unprocessable_entity unless @exercise.save
+    redirect_to @exercise, notice: "Exercise was successfully created."
+  end
+
+  def update
+    return render :edit, status: :unprocessable_entity unless @exercise.update(exercise_params)
+    redirect_to @exercise, notice: "Exercise was successfully updated."
+  end
+
+  def destroy
+    @exercise.destroy
+
+    redirect_to exercises_path, notice: "Exercise was successfully destroyed." }
+  end
+
+  private
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
+    end
+
+    def exercise_params
+      params.require(:exercise).permit(:name, :description, :intensity)
+    end
+end
