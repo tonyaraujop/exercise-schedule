59006d41f24ee6126fa6ba2ac0f1e6ecf10c19ed
Antonio Paulino <tonnyarauujo@gmail.com>
2022-03-30 09:29:23 -0300

    add confirmable and lockable features do user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :routines
   resources :exercises
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'routines#index'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    it { is_expected.to validate_length_of(:password).is_at_most(128) }
   end
 end
